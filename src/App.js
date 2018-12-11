98b24988b305200787d7087b5f2af070dc314ab7
ahmdfajri <achmad.fajria14@gmail.com>
2018-12-11 13:54:13 +0700

    versi 2

--- src/App.js
+++ src/App.js
@@ -35,14 +35,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="card mx-auto" style={{ backgroundColor: 'pink'}}>
+      <div className="container" style={{ backgroundColor: 'pink'}}>
         <div className="card text-right mx-auto mt-5" style={{width: 500, backgroundColor: 'white'}}>
           <div className="card-body">
             <h5 className="card-title">{this.state.question}</h5>
             <p className="card-text">{this.state.answer} 
             <br/>
             <i className="em em-rolling_on_the_floor_laughing"/></p>
-            <button onClick={()=>{this.punch()}} className="btn btn-success center">Reload <i className="em em-repeat "/> </button>
+            <button onClick={()=>{this.punch()}} className="btn btn-danger center">Reload <i className="em em-repeat "/> </button>
           </div>
         </div>
       </div>
